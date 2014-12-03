c5447aa3e4c47fd98fefa2d5751e2c31aca5bb06
Yusuke arakawa <yusu.arakawa@gmail.com>
2014-12-03 16:07:47 +0900

    packageの名前が長すぎるので短く変更

--- src/main/java/fillpdfform/ITextPDFForm.java
+++ src/main/java/fillpdfform/ITextPDFForm.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.yusuke.itextfillpdfform;
+package fillpdfform;
 
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.AcroFields;
